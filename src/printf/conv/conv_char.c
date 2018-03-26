e8b9d96a53c1730a231cf187d4dffaa799016110
xperrin <xperrin@student.42.fr>
2018-03-26 04:49:35 +0200

    unicode start

--- src/printf/conv/conv_char.c
+++ src/printf/conv/conv_char.c
@@ -6,19 +6,32 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 13:42:25 by xperrin           #+#    #+#             */
-/*   Updated: 2018/02/28 18:19:32 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/03/26 01:28:44 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 #include <unistd.h>
 
-size_t	conv_char(int fd, t_parg parg, va_list ap)
+static	size_t	conv_uchar(int fd, t_parg parg, va_list ap)
+{
+	unsigned int c;
+
+	c = va_arg(ap, wchar_t);
+	(void)parg.type;
+	/* ft_putstr_fd("unicode goes here", fd); */
+	write(fd, &c, 4);
+	return (42);
+}
+
+size_t			conv_char(int fd, t_parg parg, va_list ap)
 {
 	char	c;
 	size_t	i;
 
 	i = 1;
+	if (parg.type == 'C' || ft_strchr(parg.flags, 'l'))
+			return(conv_uchar(fd, parg, ap));
 	c = (parg.type == '%') ? parg.type : va_arg(ap, int);
 	if (ft_strchr(parg.flags, '-'))
 		ft_putchar_fd(c, fd);
