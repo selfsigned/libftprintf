05d920797c044bddc91b746c95396948b243ee69
xperrin <xperrin@student.42.fr>
2018-03-27 20:28:15 +0200

    Derp

--- src/printf/conv/conv_unicode.c
+++ src/printf/conv/conv_unicode.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 01:11:32 by xperrin           #+#    #+#             */
-/*   Updated: 2018/03/27 20:14:07 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/03/27 20:27:47 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	conv_unicode(unsigned char *d, unsigned int n)
 {
-	ft_printf("binary input: %b\n", n);
+	/* ft_printf("binary input: %b\n", n); */
 	if (n <= 0x7F)
 	{
 		d[0] = n;
@@ -27,7 +27,6 @@ size_t	conv_unicode(unsigned char *d, unsigned int n)
 		/* ft_printf("binary output result: %b %b\n", d[0], d[1]); */
 		return (2);
 	}
-	/* PLACEHOLDERS */
 	else if (n <= 0xFFFF)
 	{
 		d[0] = (n >> 12) + 0xE0;
